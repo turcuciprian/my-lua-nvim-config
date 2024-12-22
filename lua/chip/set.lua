ef40cef95a70ad088feed4b61cc33cffb4092ada
turcuciprian <turcuciprian1@gmail.com>
2024-12-23 00:25:17 +0200

    added surround plugin added useful keymaps for search and replace added keymap for refferences change

--- lua/chip/set.lua
+++ lua/chip/set.lua
@@ -4,11 +4,4 @@ vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
 vim.opt.shiftwidth = 2 
 vim.opt.softtabstop = 2
-vim.opt.ic = true 
---set cursorline
---set cursorcolumn
---set shiftwidth=2
---set softtabstop=2
---set relativenumber
---set ic
-
+vim.opt.ic = true
