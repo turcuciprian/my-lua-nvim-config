738b15e478836c0d2768cc681295f92067240dcc
turcuciprian <turcuciprian1@gmail.com>
2025-02-02 17:33:58 +0200

    solved day 4 part 2

--- instances/033-aoc-2020-day4/part2.py
+++ instances/033-aoc-2020-day4/part2.py
@@ -5,27 +5,27 @@ def validate_key(keyValue):
     key = keyValue[0]
     value = keyValue[1]
     if key == "byr":
-        return len(value) == 4 and 1920 <= int(value) <= 2002
+        return len(value) == 4 and value.isdigit() and 1920 <= int(value) and int(value) <= 2002
 
     if key == "iyr":
-        return len(value) == 4 and 2010 <= int(value) <= 2020
+        return len(value) == 4 and value.isdigit() and 2010 <= int(value) and int(value) <= 2020
 
     if key == "eyr":
-        return len(value) == 4 and 2020 <= int(value) <= 2030
+        return len(value) == 4 and value.isdigit() and 2020 <= int(value) and int(value) <= 2030
 
     if key == "hgt":
         hgt_cond = False
         if "cm" in value:
             height = value.split("cm")[0]
-            hgt_cond = height.isdigit() and 150 <= int(height) <= 193
+            hgt_cond = height.isdigit() and 150 <= int(height) and int(height) <= 193
         if "in" in value:
             height = value.split("in")[0]
-            hgt_cond = height.isdigit() and 59 <= int(height) <= 76
+            hgt_cond = height.isdigit() and 59 <= int(height) and int(height) <= 76
 
         # len and sum comparison
 
         has_match = [True for flag in ["cm", "in"] if flag in value]
-        return (sum(has_match) == 1) and hgt_cond
+        return (all(has_match)) and hgt_cond
 
     if key == "hcl":
         hcl_cond = (
@@ -50,11 +50,10 @@ def validate_key(keyValue):
 
     if key == "cid":
         return True
-    
-    return False
 
 
-input = open("input.txt", "r").read()
+
+input = open("input2.txt", "r").read()
 # Step 1: split text into array of passports
 passports_raw = input.split("\n\n")
 
@@ -62,7 +61,7 @@ passports_raw = input.split("\n\n")
 passports_raw = [passport.replace("\n", " ") for passport in passports_raw]
 
 # Step 3: split each key-value from passport data separated by space
-passports_list = [passport.split(" ") for passport in passports_raw]
+passports_list = [passport.strip().split(" ") for passport in passports_raw]
 
 # Step 4: split key/value passport information into sub list
 # passports = [pass_info for pass_info in passport for passport in passports_list]
@@ -75,29 +74,42 @@ for passport in passports_list:
 # Step 5: create a list with all the keys
 all_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
 valid_passports = 0
-# Step 6: check if there are 8 keys present (all the 8 keys)
-for passport in passports:
-    breakpoint()
+for index, passport in enumerate(passports):
     all_keys_present_list = [True for pass_info in passport if pass_info[0] in all_keys]
 
     # 8 keys are present
-    cond1 = sum(1 for key_exists in all_keys_present_list if key_exists) >= 7
+    cond1 = len(all_keys_present_list)==8
 
     sum_validated_passports = sum(
         [1 for key_value in passport if validate_key(key_value)]
     )
-    all_keys_are_valid = sum_validated_passports == len(passport)
+
+    validated_passports_match_length = sum_validated_passports == len(passport)
+    all_keys_are_valid = sum_validated_passports == 8 and validated_passports_match_length
+
+    #
+    # COND1 - 8 keys are present
+    #
+
     if cond1:
         if all_keys_are_valid:
             valid_passports += 1
     else:
-        # less than 8 keys are present
-        # Step 7: if Step 6 is not true, check if there are 7 keys present and if cid is present - passport invalid if not and valid if yes
+        # Step 7: check length of passport information items
         cond2 = sum(1 for key_exists in all_keys_present_list if key_exists) == 7
-        # if cid is in present the keys
-        cond3 = "cid" in [key_value[0] for key_value in passport]
 
-        if cond2 and not cond3:
-            if all_keys_are_valid:
+        key_items = list(set([key_value[0] for key_value in passport]))
+
+        # if cid is in present the keys
+        cond3 = (key_items.count("cid")) == 1
+
+        # if all keys that exist should exist
+        cond4 = set(key_items).issubset(all_keys)
+
+        #
+        # COND2 - 7 keys & COND3 cid is present
+        #
+        if cond2 and  not cond3 and cond4:
+            if validated_passports_match_length:
                 valid_passports += 1
-print("valid passports:",valid_passports)
+print("valid passports:", valid_passports)
