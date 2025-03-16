359cd4a8e06989f09d55c252486054aef5ca8ecd
turcuciprian <turcuciprian1@gmail.com>
2025-03-16 22:59:05 +0000

    added day 10_2

---
+++ advent_of_code/2020/day10_2.py
@@ -0,0 +1,15 @@
+import time
+import numpy as np
+
+with open("inputs/day10.txt") as f:
+    data = f.read().split("\n")
+    data = np.sort(np.array([0, *data], dtype=int))
+    data = np.array([*data, data[-1]+3])
+star_time = time.time()
+jolt_differences = np.array(
+    [int(data[i + 1] - data[i]) for i in range(len(data)) if len(data)-1 > i]
+)
+ones = int(np.sum(jolt_differences == 1))
+threes = int(np.sum(jolt_differences == 3))
+print(f"result: {ones * threes}")
+print(f"Lasted: {time.time()-star_time:0.5f} seconds")
