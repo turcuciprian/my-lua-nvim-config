b58adc33cbde7dec5e6caa8d5fac934618742c48
turcuciprian <turcuciprian1@gmail.com>
2025-03-18 00:23:09 +0000

    commented the code

--- advent_of_code/2020/day10_2.py
+++ advent_of_code/2020/day10_2.py
@@ -2,7 +2,9 @@ import time
 import numpy as np
 
 with open("inputs/day10.txt") as f:
+    # splitting by line
     data = f.read().split("\n")
+    # sorting ascendingly and converting to int
     data = np.sort(np.array([0, *data], dtype=int))
     data = np.array([*data, data[-1]+3])
 star_time = time.time()
