4c3ea519c099a533d4b4847eb9e3406a7c9928e3
turcuciprian <turcuciprian1@gmail.com>
2025-03-18 00:28:42 +0000

    addded requirements

--- advent_of_code/2020/day10_2.py
+++ advent_of_code/2020/day10_2.py
@@ -6,13 +6,19 @@ with open("inputs/day10.txt") as f:
     data = f.read().split("\n")
     # sorting ascendingly and converting to int
     data = np.sort(np.array([0, *data], dtype=int))
+    # converting to a np array and making the last item as per requirements +3 jolts than the previously sorted bigger
     data = np.array([*data, data[-1]+3])
+
 star_time = time.time()
+#extract the differences between the numbers
 jolt_differences = np.array(
     [int(data[i + 1] - data[i]) for i in range(len(data)) if len(data)-1 > i]
 )
+#calculate the ones in the list
 ones = int(np.sum(jolt_differences == 1))
+# calculate the 3's in the list
 threes = int(np.sum(jolt_differences == 3))
+#show the results
 print(f"result: {ones * threes}")
 print(f"Lasted: {time.time()-star_time:0.5f} seconds")
 
